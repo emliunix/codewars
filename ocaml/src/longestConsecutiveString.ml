9458f1a1a722457b505f34d4c3f1ac2974c873a0
Alan Liu <emliunix@hotmail.com>
2018-09-18 11:38:38 +0800

    longestConsecutiveString finished

--- ocaml/src/longestConsecutiveString.ml
+++ ocaml/src/longestConsecutiveString.ml
@@ -24,36 +24,39 @@ let listDrop n list =
   _rec 0 list;;
 
 let longestConsec (xs: string list) (k: int): string =
-  let maxI = List.length xs - k in
-  let rec _rec i =
-    let rs = listDrop i xs in
-    let ls = listTake k rs in
-    let str = String.concat "" ls in
-    if i < maxI then
-      str :: _rec (i + 1)
-    else
-      [str]
-  in
-  let str_ls = _rec 0 in
-  let str_len_ls = str_ls |> List.map String.length in
-  let maxi xs =
-    let rec _rec i xs maxI maxVal =
-      match xs with
-      | [] -> maxI
-      | x::xs ->
-         if x > maxVal then
-           _rec (i + 1) xs i x
-         else
-           _rec (i + 1) xs maxI maxVal
+  if List.length xs = 0 || k <= 0 || k > List.length xs then
+    ""
+  else
+    let maxI = List.length xs - k in
+    let rec _rec i =
+      let rs = listDrop i xs in
+      let ls = listTake k rs in
+      let str = String.concat "" ls in
+      if i < maxI then
+        str :: _rec (i + 1)
+      else
+        [str]
     in
-    match xs with
-    | [] -> None
-    | x::xs -> Some (_rec 1 xs 0 x)
-  in
-  let max_len_i = maxi str_len_ls in
-  match max_len_i with
-  | None -> ""
-  | Some i -> List.nth str_ls i
+    let str_ls = _rec 0 in
+    let str_len_ls = str_ls |> List.map String.length in
+    let maxi xs =
+      let rec _rec i xs maxI maxVal =
+        match xs with
+        | [] -> maxI
+        | x::xs ->
+           if x > maxVal then
+             _rec (i + 1) xs i x
+           else
+             _rec (i + 1) xs maxI maxVal
+      in
+      match xs with
+      | [] -> None
+      | x::xs -> Some (_rec 1 xs 0 x)
+    in
+    let max_len_i = maxi str_len_ls in
+    match max_len_i with
+    | None -> ""
+    | Some i -> List.nth str_ls i
 ;;
 
 module Tests = struct
