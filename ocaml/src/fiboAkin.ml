6acab8244e0bd7fb7899f46dc8e6593e6335726a
Alan Liu <emliunix@hotmail.com>
2018-09-19 12:25:31 +0800

    fibAkin finished

--- ocaml/src/fiboAkin.ml
+++ ocaml/src/fiboAkin.ml
@@ -1,21 +1,45 @@
 (* https://www.codewars.com/kata/fibo-akin *)
 let u n =
-  let arr = DynArray.make 3 in
-  [0;1;1] |> List.iter (fun i -> DynArray.add arr i);
+  let arr = Array.make (n + 1) 1 in
+  [0;1;1] |> List.iteri (fun i e -> Array.set arr i e);
   let rec _rec i =
-    if i = n then ()
+    if i = n + 1 then ()
     else
-      let n1 = DynArray.get arr (i - 2) in
-      let n2 = DynArray.get arr (i - 1) in
-      let v1 = DynArray.get arr (i - n1) in
-      let v2 = DynArray.get arr (i - n2) in
+      let n1 = Array.get arr (i - 2) in
+      let n2 = Array.get arr (i - 1) in
+      let v1 = Array.get arr (i - n1) in
+      let v2 = Array.get arr (i - n2) in
       let v = v1 + v2 in
-      Printf.printf "%d " v;
-      DynArray.add arr v;
+      (* Printf.printf "%d " v; *)
+      Array.set arr i v;
       _rec (i + 1);
   in
-  _rec 3
+  _rec 3;
+  arr
 ;;
 
+let lengthSupUK n k =
+  u n
+  |> Array.fold_left (
+         fun a i ->
+         a + if i >= k then 1 else 0
+       ) 0
+;;
 
-let () = u 1000;;
+let comp n =
+  u n
+  |> Array.fold_left (
+         fun a i ->
+         match a with
+         | (last, count) ->
+            (i, count + if i < last then 1 else 0)
+       ) (0, 0)
+  |> snd
+;;
+
+module Tests = struct
+  let%test _ = lengthSupUK 50 25 = 2;;
+  let%test _ = lengthSupUK 1745 645 = 474;;
+  let%test _ = comp 11567 = 5683;;
+  let%test _ = comp 23933 = 11856;;
+end
