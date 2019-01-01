4b893d7999d3489ef1b47f7e50f4397fa0f6f312
emliunix <emliunix@hotmail.com>
2019-01-02 02:47:06 +0800

    fix easyDiagonal build issue

--- ocaml/src/easyDiagonal.ml
+++ ocaml/src/easyDiagonal.ml
@@ -1,8 +1,8 @@
 (* https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf/ *)
 (* http://mathworld.wolfram.com/BinomialCoefficient.html *)
-open Big_int
+open Big_int_Z
 
-let diag (a:Big_int.big_int) n =
+let diag (a:big_int) n =
   let rec _r i =
     if i == 0 then [] else a :: (_r (i - 1))
   in
@@ -16,7 +16,7 @@ let trans xs =
        match rs with
        | [] -> []
        | i :: _rs ->
-          let n = (Big_int.add_big_int i p) in
+          let n = (add_big_int i p) in
           n :: (_tx n _rs)
      in
      a :: (_tx a xs);;
@@ -25,6 +25,6 @@ let rec n_trans n xs =
   if n == 0 then xs else n_trans (n - 1) (trans xs);;
 
 let diagonal (n:int) (p:int) : string =
-  let ls = (n_trans p (diag Big_int.unit_big_int (n - p + 1))) in
-  let sum = List.fold_left (fun a b -> Big_int.add_big_int a b) Big_int.zero_big_int ls in
-  Big_int.string_of_big_int sum;;
+  let ls = (n_trans p (diag unit_big_int (n - p + 1))) in
+  let sum = List.fold_left (fun a b -> add_big_int a b) zero_big_int ls in
+  string_of_big_int sum;;
